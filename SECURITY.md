02b32766db006b2d1271125b0d452a79aacd5c07
Tom Kralidis <tomkralidis@gmail.com>
2021-09-21 06:53:42 -0400

    bootstrap

---
+++ SECURITY.md
@@ -0,0 +1,15 @@
+# wis2node Security Policy
+
+## Reporting
+
+Security/vulnerability reports **should not** be submitted through GitHub issues or public discussions, but instead please send your report 
+to **tomkralidis nospam @ gmail.com** - (remove the blanks and 'nospam').  
+
+## Supported Versions
+
+wis2node developers will release patches for security vulnerabilities for the following versions:
+
+| Version | Supported          |
+| ------- | ------------------ |
+| latest stable version | :white_check_mark: |
+| previous versions | :x:                |
---
+++ setup.py
@@ -0,0 +1,115 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+import io
+import os
+import re
+from setuptools import Command, find_packages, setup
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import subprocess
+        errno = subprocess.call(['pytest'])
+        raise SystemExit(errno)
+
+
+def read(filename, encoding='utf-8'):
+    """read file contents"""
+    full_path = os.path.join(os.path.dirname(__file__), filename)
+    with io.open(full_path, encoding=encoding) as fh:
+        contents = fh.read().strip()
+    return contents
+
+
+def get_package_version():
+    """get version from top-level package init"""
+    version_file = read('wis2node/__init__.py')
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+KEYWORDS = [
+    'wmo',
+    'wis 2.0'
+]
+
+DESCRIPTION = 'WIS 2.0 node in a box provides '
+
+DESCRIPTION = ('WIS 2.0 node in a box provides a platform with the '
+               'capabilities for centres to integrate their data holdings '
+               'and publish them to the WMO Information System with a plug '
+               'and play capability supporting data publishing, discovery and '
+               'access.')
+
+
+# ensure a fresh MANIFEST file is generated
+if (os.path.exists('MANIFEST')):
+    os.unlink('MANIFEST')
+
+
+setup(
+    name='wis2node',
+    version=get_package_version(),
+    description=DESCRIPTION.strip(),
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    platforms='all',
+    keywords=' '.join(KEYWORDS),
+    author='Tom Kralidis',
+    author_email='tomkralidis@gmail.com',
+    maintainer='Tom Kralidis',
+    maintainer_email='tomkralidis@gmail.com',
+    url='https://github.com/wmo-im/wis2node',
+    install_requires=read('requirements.txt').splitlines(),
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'wis2node=wis2node:cli'
+        ]
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        'Topic :: Scientific/Engineering :: GIS',
+        'Topic :: Scientific/Engineering :: Information Analysis'
+    ],
+    cmdclass={'test': PyTest},
+    test_suite='tests.run_tests'
+)
