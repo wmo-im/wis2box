f1dd63cb900edaf27fa3f2f9898de5c9b45a4e8a
PeterSilva <peter@bsqt.homeip.net>
2022-02-03 19:59:08 -0500

    changeing baseURL values to be correct. adjusting event handlers to suit.

--- wis2node/event/data_ingest.py
+++ wis2node/event/data_ingest.py
@@ -39,8 +39,8 @@ class Event(FlowCB):
         new_incoming = []
         for incoming_message in worklist.incoming:
 
-            filepath = (Path(incoming_message['baseUrl']) /
-                        incoming_message['relPath'])
+            filepath = ( Path( '/' ) / incoming_message['relPath'])
+
             LOGGER.debug(f'Incoming filepath: {filepath}')
 
             try:
