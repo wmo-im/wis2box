06c28cca3ffbd5244d52879b15214123295f9ab5
Tom Kralidis <tomkralidis@gmail.com>
2022-02-09 11:13:06 +0000

    derive mosquitto credentials from WIS2NODE_BROKER

--- docker/mosquitto/entrypoint.sh
+++ docker/mosquitto/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-# FIXME: derive credentials from env
-mosquitto_passwd -b -c /mosquitto/config/password.txt wis2node wis2node
+# derive credentials from WIS2NODE_BROKER
+CREDENTIALS=`echo $WIS2NODE_BROKER | awk -F/ '{print $3}' | awk -F@ '{print $1}' | sed 's/:/ /'`
+
+mosquitto_passwd -b -c /mosquitto/config/password.txt $CREDENTIALS
 
 /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
