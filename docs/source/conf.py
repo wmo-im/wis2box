cb58895e97ff278d483869eb0ca2b66eb675bb71
Tom Kralidis <tomkralidis@gmail.com>
2022-02-14 15:21:54 -0500

    fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -81,7 +81,6 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'sphinx_wmo_theme'
 html_theme = 'nature'
 
 # Add any paths that contain custom static files (such as style sheets) here,
