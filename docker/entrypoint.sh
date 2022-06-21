adc459e465fd992b1b4049ef1bf4f4ce32bd5188
Tom Kralidis <tomkralidis@gmail.com>
2022-06-21 15:14:26 -0400

    fix

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,17 +26,16 @@ echo "START /entrypoint.sh"
 
 set +e
 
+env
+
 wis2box environment create
-if test -f "${WIS2BOX_API_CONFIG}"; then
-    echo "${WIS2BOX_API_CONFIG} already exists."
+if test -f "$WIS2BOX_API_CONFIG"; then
+    echo "$WIS2BOX_API_CONFIG already exists."
 else
-    echo "Creating ${WIS2BOX_API_CONFIG}."
-    cp /wis2box-api/wis2box-api-config.yml ${WIS2BOX_API_CONFIG}
+    echo "Creating $WIS2BOX_API_CONFIG."
+    cp /wis2box-api/wis2box-api-config.yml $WIS2BOX_API_CONFIG
 fi
 
-if [ ! -d "${WIS2BOX_DATADIR}/config/csv2bufr" ]; then
-  git clone https://github.com/wmo-im/csv2bufr-templates.git ${WIS2BOX_DATADIR}/config/csv2bufr
+if [ ! -d "$WIS2BOX_DATADIR/config/csv2bufr" ]; then
+  git clone https://github.com/wmo-im/csv2bufr-templates.git $WIS2BOX_DATADIR/config/csv2bufr
 fi
-
-#sr3 --logStdout start
-#sleep infinity
