25bb966c6ad1b934adc00d3372704334460953dd
Tom Kralidis <tomkralidis@gmail.com>
2022-07-04 22:38:00 -0400

    add storage ABC

---
+++ wis2box/storage/base.py
@@ -0,0 +1,90 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+from enum import Enum
+import logging
+from pathlib import Path
+from typing import Any
+
+LOGGER = logging.getLogger(__name__)
+
+
+class StorageTypes(Enum):
+    FS = 'fs'
+    S3 = 's3'
+
+
+class StorageBase:
+    """Abstract storage manager"""
+    def __init__(self, storage_type: StorageTypes, source: str,
+                 name: str = None, auth: dict = {}) -> None:
+        """
+        DataSource initializer
+
+        :param storage_type: type of storage
+        :param source: URL of storage service, or basepath of filesystem
+        :param name: bucket or container name (can be None if not applicable)
+        :param auth: `dict` of auth parameters (specific to provider)
+
+        :returns: `None`
+        """
+
+        self.storage_type = storage_type
+        self.source = source
+        self.name = name
+        self.auth = auth
+
+    def get(self, identifier: str) -> Any:
+        """
+        Access data source from storage
+
+        :param identifier: `str` of data source identifier
+
+        :returns: object result
+        """
+
+        raise NotImplementedError()
+
+    def put(self, filepath: Path, identifier: str) -> bool:
+        """
+        Access data source from storage
+
+        :param filepath: `Path` of file to upload
+        :param identifier: `str` of data source identifier
+
+        :returns: `bool` of put result
+        """
+
+        raise NotImplementedError()
+
+    def delete(self, identifier: str) -> bool:
+        """
+        Delete data source from storage
+
+        :param identifier: `str` of data source identifier
+
+        :returns: `bool` of put result
+        """
+
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return f'<StorageBase ({self.source})>'
---
+++ wis2box/storage/s3.py
@@ -0,0 +1,66 @@
+###############################################################################
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+###############################################################################
+
+import logging
+from pathlib import Path
+from typing import Any
+
+import boto3
+
+from wis2box.storage.base import StorageBase, StorageTypes
+
+LOGGER = logging.getLogger(__name__)
+
+
+class S3Storage(StorageBase):
+    """Abstract storage manager"""
+    def __init__(self, storage_type: StorageTypes, source: str,
+                 name: str = None, auth: dict = {}) -> None:
+
+        super().__init__('s3', source, name, auth)
+
+        self.client = boto3.client('s3', endpoint_url=self.source,
+                                   aws_access_key_id=self.auth['username'],
+                                   aws_secret_access_key=self.auth['password'])
+
+    def get(self, identifier: str) -> Any:
+
+        LOGGER.debug(f'Getting object {identifier}')
+        data = self.client.get_object(Bucket=self.name, Key=identifier)
+
+        return data['Body'].read()
+
+    def put(self, filepath: Path, identifier: str) -> bool:
+
+        LOGGER.debug(f'Putting file {filepath} to {identifier}')
+        self.client.upload_file(filepath, self.name, identifier)
+
+        return True
+
+    def delete(self, identifier: str) -> bool:
+
+        LOGGER.debug(f'Deleting object {identifier}')
+        self.client.delete_object(Bucket=self.name, Key=identifier)
+
+        return True
+
+    def __repr__(self):
+        return f'<S3Storage ({self.source})>'
