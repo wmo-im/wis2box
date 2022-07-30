d67d2b346ad818afca3daa2e69f184dff56c17c0
Tom Kralidis <tomkralidis@gmail.com>
2022-07-29 22:58:59 -0400

    fix ref

--- wis2box/pubsub/subscribe.py
+++ wis2box/pubsub/subscribe.py
@@ -26,7 +26,8 @@ from pathlib import Path
 import click
 
 from wis2box import cli_helpers
-from wis2box.env import BROKER, STORAGE_SOURCE
+from wis2box.env import (BROKER_HOST, BROKER_USERNAME, BROKER_PASSWORD,
+                         STORAGE_SOURCE)
 from wis2box.handler import Handler
 from wis2box.plugin import load_plugin, PLUGINS
 
@@ -74,7 +75,7 @@ def subscribe(ctx, broker, topic, verbosity):
 
     defs = {
         'codepath': PLUGINS['pubsub']['mqtt']['plugin'],
-        'url': BROKER
+        'url': f'mqtt://{BROKER_USERNAME}:{BROKER_PASSWORD}@{BROKER_HOST}'
     }
 
     broker = load_plugin('pubsub', defs)
