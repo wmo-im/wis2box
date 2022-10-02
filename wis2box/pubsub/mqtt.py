c93d90c9099ac75be4881bad6e4468de8dd7cb45
Tom Kralidis <tomkralidis@gmail.com>
2022-10-02 09:56:55 -0400

    add disconnect logging to MQTT broker (#274)
    * add disconnect logging to MQTT broker
    
    * add disconnect logging to MQTT broker
    
    * fix ref

--- wis2box/pubsub/mqtt.py
+++ wis2box/pubsub/mqtt.py
@@ -99,11 +99,19 @@ class MQTTPubSubClient(BasePubSubClient):
 
         :returns: `None`
         """
-        def on_connect(client, userdata, flags, rc):
-            client.subscribe(topic)
 
-        LOGGER.debug(f'Subscribing to broker {self.broker}')
+        def on_connect(client, userdata, flags, rc):
+            LOGGER.debug('Connected to broker {self.broker}')
+            LOGGER.debug('Subscribing to topic {topic} ')
+            client.subscribe(topic)
+            LOGGER.debug('Subscribed to topic {topic}')
+
+        def on_disconnect(client, userdata, rc):
+            LOGGER.debug('Disconnected from {self.broker}')
+
+        LOGGER.debug(f'Subscribing to broker {self.broker}, topic {topic}')
         self.conn.on_connect = on_connect
+        self.conn.on_disconnect = on_disconnect
         self.conn.loop_forever()
 
     def bind(self, event: str, function: Callable[..., Any]) -> None:
