f368361e12b3e44f819cc0af16c614a34a7bec7f
Tom Kralidis <tomkralidis@gmail.com>
2023-02-02 06:57:51 -0500

    fix

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -94,7 +94,7 @@ def test_metadata_discovery_publish():
 
     assert r['geometry'] == geometry
 
-    mqtt_link = [d for d in r['links'] if d['type'] == 'MQTT']
+    mqtt_link = [d for d in r['links'] if d['type'] == 'MQTT'][0]
 
     assert 'everyone:everyone' in mqtt_link['href']
     assert mqtt_link['channel'] == 'origin/a/wis2/mwi/mwi_met_centre/data/core/weather/surface-based-observations/synop'  # noqa
