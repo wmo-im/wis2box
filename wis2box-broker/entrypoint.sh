1c344b1a7174f3c863bdfa3c4e5f36c8e8dbdd53
Maaike <maaike.limper@gmail.com>
2023-08-23 16:31:17 +0200

    enable adding of mosquitto users (#495)
    * enable adding of mosquitto users
    
    * Update configuration.rst
    
    ---------
    
    Co-authored-by: Tom Kralidis <tomkralidis@gmail.com>

--- wis2box-broker/entrypoint.sh
+++ wis2box-broker/entrypoint.sh
@@ -7,14 +7,19 @@ if [ -f /tmp/wis2box.crt ]; then
     cp /tmp/wis2box.crt /mosquitto/certs
     cp /tmp/wis2box.key /mosquitto/certs
     chown -R mosquitto:mosquitto /mosquitto/certs
-    cp /mosquitto/config/mosquitto-ssl.conf /mosquitto/config/mosquitto.conf
+    cp -f /mosquitto/config/mosquitto-ssl.conf /mosquitto/config/mosquitto.conf
 else
     echo "SSL disabled"
 fi
 
 echo "Setting mosquitto authentication"
-mosquitto_passwd -b -c /mosquitto/config/password.txt $WIS2BOX_BROKER_USERNAME $WIS2BOX_BROKER_PASSWORD
-mosquitto_passwd -b /mosquitto/config/password.txt everyone everyone
+if [ ! -e "/mosquitto/config/password.txt" ]; then
+    echo "Adding wis2box users to mosquitto password file"
+    mosquitto_passwd -b -c /mosquitto/config/password.txt $WIS2BOX_BROKER_USERNAME $WIS2BOX_BROKER_PASSWORD
+    mosquitto_passwd -b /mosquitto/config/password.txt everyone everyone
+else
+    echo "Mosquitto password file already exists. Skipping wis2box user addition."
+fi
 
 sed -i "s#_WIS2BOX_BROKER_QUEUE_MAX#$WIS2BOX_BROKER_QUEUE_MAX#" /mosquitto/config/mosquitto.conf
 sed -i "s#_WIS2BOX_BROKER_USERNAME#$WIS2BOX_BROKER_USERNAME#" /mosquitto/config/acl.conf
