f5350d58f2fc3a94e630d2449aa1248db1f537d6
maaikelimper <maaike.limper@gmail.com>
2023-09-18 09:31:05 +0000

    undo csv2bufr change

--- wis2box-management/wis2box/data/csv2bufr.py
+++ wis2box-management/wis2box/data/csv2bufr.py
@@ -19,6 +19,7 @@
 #
 ###############################################################################
 
+import json
 import logging
 from pathlib import Path
 from typing import Union
@@ -27,13 +28,13 @@ from csv2bufr import transform as transform_csv
 import csv2bufr.templates as c2bt
 
 from wis2box.data.base import BaseAbstractData
-from wis2box.api import execute_process
+from wis2box.metadata.station import get_valid_wsi
 
 LOGGER = logging.getLogger(__name__)
 
 
 class ObservationDataCSV2BUFR(BaseAbstractData):
-    """Synoptic observation data"""
+    """Observation data"""
     def __init__(self, defs: dict) -> None:
         """
         ObservationDataCSV2BUFR data initializer
@@ -57,8 +58,7 @@ class ObservationDataCSV2BUFR(BaseAbstractData):
                 raise Exception(f'Unknown template: {self.template}, options are: {c2bt.list_templates()}') # noqa
             self.mappings['bufr4'] = c2bt.load_template(self.template)
 
-        LOGGER.debug('skip publish for call_synop_publish-plugin')
-        return True
+        self.station_metadata = None
 
     def transform(self, input_data: Union[Path, bytes],
                   filename: str = '') -> bool:
@@ -69,25 +69,46 @@ class ObservationDataCSV2BUFR(BaseAbstractData):
             LOGGER.debug('input_data is a Path')
             filename = input_data.name
 
-        file_match = self.validate_filename_pattern(filename)
-
-        if file_match is None:
+        if self.validate_filename_pattern(filename) is None:
             msg = f'Invalid filename format: {filename} ({self.file_filter})'
             LOGGER.error(msg)
             raise ValueError(msg)
 
+        LOGGER.debug('Generating BUFR4')
         input_bytes = self.as_bytes(input_data)
 
-        process_name = 'wis2box-csv2bufr'
-        # execute process
-        inputs = {
-            "data": input_bytes.decode(),
-            "template": self.template,
-            "channel": self.topic_hierarchy.dirpath,
-            "notify": self.enable_notification
-        }
-        result = execute_process(process_name, inputs)
-        if 'errors' in result:	
-            for error in result['errors']:
-                LOGGER.error(f'{process_name} error: {error}')
+        LOGGER.debug('Transforming data')
+        results = transform_csv(input_bytes.decode(),
+                                self.mappings['bufr4'])
+
+        LOGGER.debug('Iterating over BUFR messages')
+        for item in results:
+            wsi = item['_meta']['properties']['wigos_station_identifier']
+            if 'result' in item['_meta']:
+                if item['_meta']['result']['code'] != 1:
+                    msg = item['_meta']['result']['message']
+                    LOGGER.error(f'Transform returned {msg} for wsi={wsi}')
+                    self.publish_failure_message(
+                        description='csv2bufr transform error',
+                        wsi=wsi)
+                    continue
+            if get_valid_wsi(wsi) is None:
+                msg = f'Station {wsi} not in station list; skipping'
+                LOGGER.error(msg)
+                self.publish_failure_message(
+                        description='Station not in station list',
+                        wsi=wsi)
+                continue
+            LOGGER.debug('Setting obs date for filepath creation')
+            identifier = item['_meta']['id']
+            data_date = item['_meta']['properties']['datetime']
+
+            self.output_data[identifier] = item
+            self.output_data[identifier]['_meta']['relative_filepath'] = \
+                self.get_local_filepath(data_date)
+
         return True
+
+    def get_local_filepath(self, date_):
+        yyyymmdd = date_.strftime('%Y-%m-%d')
+        return (Path(yyyymmdd) / 'wis' / self.topic_hierarchy.dirpath)
