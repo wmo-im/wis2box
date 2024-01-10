1a97d2837ee21484ea42e1017bcb5b782e057879
Maaike <maaike.limper@gmail.com>
2024-01-10 15:42:43 +0100

    WIS2BOXSubscriber class using get_data_mappings

--- wis2box-management/wis2box/pubsub/subscribe.py
+++ wis2box-management/wis2box/pubsub/subscribe.py
@@ -30,6 +30,7 @@ import click
 from wis2box.api import upsert_collection_item
 from wis2box import cli_helpers
 from wis2box.api import setup_collection
+from wis2box.data_mappings import get_data_mappings
 from wis2box.env import (BROKER_HOST, BROKER_PORT, BROKER_USERNAME,
                          BROKER_PASSWORD, STORAGE_SOURCE, STORAGE_ARCHIVE)
 from wis2box.handler import Handler, NotHandledError
@@ -39,55 +40,70 @@ from wis2box.pubsub.message import gcm
 LOGGER = logging.getLogger(__name__)
 
 
-def handle(filepath):
-    try:
-        LOGGER.info(f'Processing {filepath}')
-        handler = Handler(filepath)
-        if handler.handle():
-            LOGGER.info('Data processed')
-            for plugin in handler.plugins:
-                for filepath in plugin.files():
-                    LOGGER.info(f'Public filepath: {filepath}')
-    except NotHandledError as err:
-        msg = f'not handled error: {err}'
-        LOGGER.debug(msg)
-    except ValueError as err:
-        msg = f'handle() error: {err}'
-        LOGGER.error(msg)
-    except Exception as err:
-        msg = f'handle() error: {err}'
-        raise err
+class WIS2BoxSubscriber:
 
+    def __init__(self, broker):
+        self.data_mappings = get_data_mappings()
+        self.broker = broker
+        self.broker.bind('on_message', self.on_message_handler)
+        self.broker.sub('wis2box/#')
 
-def on_message_handler(client, userdata, msg):
-    LOGGER.debug(f'Raw message: {msg.payload}')
+    def handle(self, filepath, message):
+        try:
+            LOGGER.info(f'Processing {message["EventName"]} for {filepath}')
+            # load handler
+            handler = Handler(filepath=filepath,
+                              data_mappings=self.data_mappings)
+            if handler.handle():
+                LOGGER.info('Data processed')
+                for plugin in handler.plugins:
+                    for filepath in plugin.files():
+                        LOGGER.info(f'Public filepath: {filepath}')
+        except NotHandledError as err:
+            msg = f'not handled error: {err}'
+            LOGGER.debug(msg)
+        except ValueError as err:
+            msg = f'handle() error: {err}'
+            LOGGER.error(msg)
+        except Exception as err:
+            msg = f'handle() error: {err}'
+            raise err
 
-    topic = msg.topic
-    message = json.loads(msg.payload)
-    LOGGER.info(f'Incoming message on topic {topic}')
-    if topic == 'wis2box/notifications':
-        LOGGER.info(f'Notification: {message}')
-        # store notification in messages collection
-        upsert_collection_item('messages', message)
-    else:
-        if message.get('EventName') == 's3:ObjectCreated:Put':
-            LOGGER.debug('Incoming data is an s3 data object')
-            key = str(message['Key'])
-            filepath = f'{STORAGE_SOURCE}/{key}'
-            if key.startswith(STORAGE_ARCHIVE):
-                LOGGER.info(f'Do not process archived-data: {key}')
-                return
-        elif 'relPath' in message:
-            LOGGER.debug('Incoming data is a filesystem path')
-            filepath = Path(message['relPath'])
+    def on_message_handler(self, client, userdata, msg):
+        LOGGER.debug(f'Raw message: {msg.payload}')
+
+        topic = msg.topic
+        message = json.loads(msg.payload)
+        LOGGER.info(f'Incoming message on topic {topic}')
+        filepath = None
+        if topic == 'wis2box/notifications':
+            LOGGER.info(f'Notification: {message}')
+            # store notification in messages collection
+            upsert_collection_item('messages', message)
         else:
-            LOGGER.debug('ignore message')
-            return
+            if message.get('EventName') == 's3:ObjectCreated:Put':
+                LOGGER.debug('Received s3:ObjectCreated:Put')
+                key = str(message['Key'])
+                filepath = f'{STORAGE_SOURCE}/{key}'
+                if key.startswith(STORAGE_ARCHIVE):
+                    LOGGER.info(f'Do not process archived-data: {key}')
+                    return
+            elif 'relPath' in message:
+                LOGGER.debug('Received relPath')
+                filepath = Path(message['relPath'])
+                message['EventName'] = 'FilePathReceived'
+            elif message.get('EventName') == 'ReloadMappingRequest':
+                LOGGER.debug('Received ReloadMappingRequest')
+                self.data_mappings = get_data_mappings()
+                return
+            else:
+                LOGGER.debug('ignore message')
+                return
 
-        while len(mp.active_children()) == mp.cpu_count():
-            sleep(0.1)
-        p = mp.Process(target=handle, args=(filepath,))
-        p.start()
+            while len(mp.active_children()) == mp.cpu_count():
+                sleep(0.1)
+            p = mp.Process(target=self.handle, args=(filepath, message))
+            p.start()
 
 
 @click.command()
@@ -106,7 +122,6 @@ def subscribe(ctx, verbosity):
 
     broker = load_plugin('pubsub', defs)
 
-    broker.bind('on_message', on_message_handler)
-
-    click.echo('Subscribing to internal broker on topic wis2box/#')
-    broker.sub('wis2box/#')
+    # start the wis2box subscriber
+    click.echo('Starting WIS2Box subscriber')
+    WIS2BoxSubscriber(broker=broker)
