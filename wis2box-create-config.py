0c3e486f69755f4a7cd9c0859f648cb28c4496f1
Maaike <mlimper@wmo.int>
2024-06-14 11:16:47 +0200

    Update wis2box-create-config.py, use websockets and port=443 as default for downloader

--- wis2box-create-config.py
+++ wis2box-create-config.py
@@ -361,11 +361,11 @@ def create_wis2box_env(config_dir: str) -> None:
         # downloader settings
         fh.write('# downloader settings\n')
         fh.write('DOWNLOAD_BROKER_HOST=globalbroker.meteo.fr\n')
-        fh.write('DOWNLOAD_BROKER_PORT=8883\n')
+        fh.write('DOWNLOAD_BROKER_PORT=443\n')
         fh.write('DOWNLOAD_BROKER_USERNAME=everyone\n')
         fh.write('DOWNLOAD_BROKER_PASSWORD=everyone\n')
         fh.write('# download transport mechanism (tcp or websockets)\n')
-        fh.write('DOWNLOAD_BROKER_TRANSPORT=tcp\n')
+        fh.write('DOWNLOAD_BROKER_TRANSPORT=websockets\n')
         fh.write('# maximum MB in download directory\n')
         fh.write('DOWNLOAD_MIN_FREE_SPACE_GB=1\n')
         fh.write('\n')
