cbdbba633645a0d79b85d5d996d87986fed8ac13
Maaike <maaike.limper@gmail.com>
2024-06-21 14:17:01 +0200

    remove downloader CLI

--- wis2box-management/wis2box/__init__.py
+++ wis2box-management/wis2box/__init__.py
@@ -30,7 +30,6 @@ from wis2box.env import environment
 from wis2box.metadata import metadata
 from wis2box.pubsub import pubsub
 from wis2box.auth import auth
-from wis2box.downloader import downloader
 
 
 @click.group()
@@ -47,4 +46,3 @@ cli.add_command(dataset)
 cli.add_command(metadata)
 cli.add_command(auth)
 cli.add_command(pubsub)
-cli.add_command(downloader)
