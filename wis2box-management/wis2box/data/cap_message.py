5f61246f9bca6800308411ccf988b88f2a3e9136
RoryPTB <47696929+RoryPTB@users.noreply.github.com>
2024-07-18 17:06:49 +0200

    feat: use the capvalidator date extractor to add sent date to metadata

--- wis2box-management/wis2box/data/cap_message.py
+++ wis2box-management/wis2box/data/cap_message.py
@@ -26,7 +26,7 @@ from datetime import datetime
 from typing import Union
 
 from wis2box.data.base import BaseAbstractData
-from capvalidator import validate_xml
+from capvalidator import validate_xml, get_dates
 
 LOGGER = logging.getLogger(__name__)
 
@@ -64,17 +64,15 @@ class CAPMessageData(BaseAbstractData):
         rmk = filename.split('.')[0]
         input_bytes = self.as_bytes(input_data)
 
-        xml_content = self.as_string(input_bytes)
-        LOGGER.info(
-            f'Received CAP with the following content:\n {xml_content}')
-
+        # get the sent date from the CAP XML
+        sent_date = get_dates(input_bytes).sent
         # convert isoformat to datetime
-        self._meta['data_date'] = datetime.fromisoformat(self._meta['data_date'])  # noqa
+        self._meta['data_date'] = datetime.fromisoformat(sent_date)
         # add relative filepath to _meta
         self._meta['relative_filepath'] = self.get_local_filepath(self._meta['data_date'])  # noqa
 
         # validate the CAP XML string content using the capvalidator package
-        result = validate_xml(xml_content)
+        result = validate_xml(input_bytes)
         if not result.passed:
             LOGGER.error(
                 f'Invalid CAP XML, not publishing. Reason: {result.message}')
