0df17d604df8db8f3e16ad5b750e369bf488aa0d
Maaike <mlimper@wmo.int>
2025-01-22 03:00:54 +0100

    use docker-compose-images file from separate repo to define images (#844)
    * use docker-compose-images file
    
    * add zaproxy fix to new PR
    
    * remove useless print statement and f string
    
    * use wmo-im/wis2box-release
    
    * update remove_old_docker_images and "--restart" option to "update"
    
    * return options or exit, add install packages
    
    * implemented changes requested by Tom
    
    * remove from packaging.version import Version
    
    * add user-agent
    
    * updates to get_resolved_version to work with wis2box-api
    
    * add function fetch_data_from_url to deal with redirects
    
    * Update wis2box-ctl.py
    
    ---------
    
    Co-authored-by: Tom Kralidis <tomkralidis@gmail.com>

--- wis2box-ctl.py
+++ wis2box-ctl.py
@@ -21,8 +21,12 @@
 ###############################################################################
 
 import argparse
+import glob
+import http.client
+import json
 import os
 import subprocess
+from urllib.parse import urlparse
 
 if subprocess.call(['docker', 'compose'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL) > 0:
     DOCKER_COMPOSE_COMMAND = 'docker-compose'
@@ -37,8 +41,10 @@ DOCKER_COMPOSE_ARGS = """
     --project-name wis2box_project
     """
 
+GITHUB_RELEASE_REPO = 'wmo-im/wis2box-release'
+
 parser = argparse.ArgumentParser(
-    description='manage a compposition of docker containers to implement a wis 2 box',
+    description='manage a composition of docker containers to implement a WIS2-in-a-box',
     formatter_class=argparse.RawTextHelpFormatter)
 
 parser.add_argument(
@@ -133,6 +139,180 @@ def run(cmd, silence_stderr=False) -> None:
     return None
 
 
+def fetch_data_from_url(url: str, headers: dict = {}) -> bytes:
+    """
+    Fetch data from a given URL.
+
+    :param url: required, string. URL to fetch data from.
+    :param headers: optional, dict. Headers to send with the request.
+
+    :returns: bytes. The response data.
+    """
+
+    parsed_url = urlparse(url)
+    full_path = parsed_url.path
+    if parsed_url.query:  # Include query string if it exists
+        full_path += '?' + parsed_url.query
+    try:
+        conn = http.client.HTTPSConnection(parsed_url.netloc)
+        conn.request("GET", full_path, headers=headers)
+        response = conn.getresponse()
+        # Follow redirect if status is 301 or 302
+        if response.status in (301, 302):
+            location = response.getheader('Location')
+            if location:
+                conn.close()
+                return fetch_data_from_url(location, headers)
+        elif response.status != 200:
+            print(f"Error fetching URL: {response.status}")
+            exit(1)
+        return response.read()
+    except Exception as e:
+        print(f"Error fetching URL: {e}")
+        exit(1)
+
+def get_resolved_version() -> str:
+    """
+    Determine the latest matching release tag from the wis2box-release repository.
+
+    :return: The latest release tag or an error message if not found.
+    """
+
+    if not os.path.exists('VERSION.txt'):
+        print('VERSION.txt file does not exist')
+        exit(1)
+    with open('VERSION.txt') as f:
+        base_version = f.readline().strip()
+    
+    if base_version == 'LOCAL_BUILD':
+        return base_version
+    # otherwise, fetch the latest release tag from GITHUB_RELEASE_REPO
+    url = f'https://api.github.com/repos/{GITHUB_RELEASE_REPO}/releases'
+    headers = {
+        'Accept': 'application/vnd.github.v3+json',
+        'User-Agent': 'wis2box'
+    }
+
+    options = []
+    data = fetch_data_from_url(url, headers)
+    releases = json.loads(data.decode('utf-8'))
+    for release in releases:
+        if base_version in release['tag_name']:
+            options.append(release['tag_name'])
+
+    if options:
+        # Sort options by the patch version (assuming format vX.Y.Z)
+        def extract_patch_version(tag):
+            try:
+                # Split the version string by dots after removing the 'v' prefix
+                parts = tag.lstrip('v').split('.')
+                # Return the patch version as an integer (last part)
+                return int(parts[2]) if len(parts) > 2 else 0
+            except (ValueError, IndexError):
+                # malformed version string are sorted last
+                return -1
+        options.sort(key=extract_patch_version, reverse=True)
+        return options[0]
+    else:
+        print(f'No matching versions found for VERSION.txt={base_version}')
+        exit(1)
+
+
+def remove_old_docker_images() -> None:
+    """
+    Remove any image in docker-compose.images-*.yml.bak
+    that is not in docker-compose.images-*.yml
+
+    :return: None.
+    """
+
+    old_images = []
+    new_images = []
+    
+    docker_image_files_old = glob.glob('docker-compose.images-*.yml.bak')
+    for file_ in docker_image_files_old:
+        with open(file_) as f:
+            for line in f:
+                if 'image:' in line:
+                    old_images.append(line.split(':', 1)[1].strip())
+
+    docker_image_files_new = glob.glob('docker-compose.images-*.yml')
+    for file_ in docker_image_files_new:
+        with open(file_) as f:
+            for line in f:
+                if 'image:' in line:
+                    new_images.append(line.split(':', 1)[1].strip())
+
+    for image in old_images:
+        if image not in new_images:
+            print(f'Removing {image}')
+            subprocess.run(['docker', 'rmi', image], stderr=subprocess.DEVNULL)
+    
+    # ask user to remove the old docker-compose.images-*.yml.bak files
+    for file_ in docker_image_files_old:
+        query = f'Remove {file_} ? (y/n)'
+        print(query)
+        response = input()
+        while response not in ['y', 'n']:
+            print(query)
+            response = input()
+        if response == 'y':
+            os.remove(file_)
+
+def update_images_yml() -> str:
+    """
+
+    Update the docker-compose.images-*.yml file to the latest release tag.
+
+    :return: The latest release tag or an error message if not found.
+    """
+    
+    # get the latest release tag
+    version = get_resolved_version()
+    if version == 'LOCAL_BUILD':
+        url_host = 'raw.githubusercontent.com'
+        url_path = f'/{GITHUB_RELEASE_REPO}/refs/heads/main/docker-compose.images.yml'
+    else:
+        url_host = 'github.com'
+        url_path = f'/{GITHUB_RELEASE_REPO}/releases/download/{version}/docker-compose.images.yml'
+
+    current_version = 'Undefined'
+    # find currently used version of docker-compose.images-*.yml
+    for file in os.listdir('.'):
+        if file.startswith('docker-compose.images-') and file.endswith('.yml'):
+            current_version = file.split('-')[2].split('.')[0]
+
+    if current_version == version:
+        print(f'Using latest version {version}, no update of images file required')
+        return
+    
+    if version not in ['LOCAL_BUILD', 'Undefined']:
+        # ask the user if they want to update the docker-compose.images-*.yml file
+        print(f'Current version={current_version}, latest version={version}')
+        query = f'Would you like to update ? (y/n/exit)'
+        print(query)
+        response = input()
+        while response not in ['y', 'n', 'exit']:
+            print(query)
+            response = input()
+        if response == 'n':
+            return
+        if response == 'exit':
+            exit(0)
+        if response == 'y':
+            print(f'Updating wis2box to version {version}')
+    
+    # download the file and store as docker-compose.images-{version}.yml
+    data = fetch_data_from_url(f'https://{url_host}{url_path}')
+    with open(f'docker-compose.images-{version}.yml', 'wb') as f:
+        f.write(data)
+    
+    # rename any other docker-compose.images-*.yml file to .bak
+    image_files = glob.glob('docker-compose.images-*.yml')
+    for file in image_files:
+        if file != f'docker-compose.images-{version}.yml':
+            os.rename(file, file + '.bak')
+    
 def make(args) -> None:
     """
     Serves as pseudo Makefile using Python subprocesses.
@@ -148,13 +328,22 @@ def make(args) -> None:
     # check if WIS2BOX_SSL_KEY and WIS2BOX_SSL_CERT are set
     ssl_key = None
     ssl_cert = None
-    with open('wis2box.env', 'r') as f:
+    with open('wis2box.env') as f:
         for line in f:
             if 'WIS2BOX_SSL_KEY' in line:
                 ssl_key = line.split('=')[1].strip()
             if 'WIS2BOX_SSL_CERT' in line:
                 ssl_cert = line.split('=')[1].strip()
-    docker_compose_args = DOCKER_COMPOSE_ARGS
+
+    if not glob.glob('docker-compose.images-*.yml'):
+        print("No docker-compose.images-*.yml files found, creating one")
+        update_images_yml()
+        # update docker_compose_args with the latest docker-compose.images-*.yml file
+        docker_image_file = glob.glob('docker-compose.images-*.yml')[0]
+        docker_compose_args = DOCKER_COMPOSE_ARGS + f' --file {docker_image_file}'
+
+    docker_image_file = glob.glob('docker-compose.images-*.yml')[0]
+    docker_compose_args = DOCKER_COMPOSE_ARGS + f' --file {docker_image_file}'
     if args.ssl or (ssl_key and ssl_cert):
         docker_compose_args +=" --file docker-compose.ssl.yml"
     if args.ssl and not (ssl_key and ssl_cert):
@@ -172,6 +361,7 @@ def make(args) -> None:
         run(split(
             f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} build {containers}'))
     elif args.command in ["up", "start", "start-dev"]:
+        # if no docker-compose.images-*.yml files exist, run get_images()
         run(split(
             'docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions'),
             silence_stderr=True)
@@ -183,6 +373,7 @@ def make(args) -> None:
                 run(split(f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} --file docker-compose.dev.yml up -d'))
             else:
                 run(split(f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} up -d'))
+                remove_old_docker_images()
     elif args.command == "execute":
         run(['docker', 'exec', '-i', 'wis2box-management', 'sh', '-c', containers])
     elif args.command == "login":
@@ -199,7 +390,18 @@ def make(args) -> None:
             run(split(
                 f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} down --remove-orphans {containers}'))
     elif args.command == "update":
+        update_images_yml()
+        # update docker_compose_args with the latest docker-compose.images-*.yml file
+        docker_image_file = glob.glob('docker-compose.images-*.yml')[0]
+        docker_compose_args = DOCKER_COMPOSE_ARGS + f' --file {docker_image_file}'
         run(split(f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} pull'))
+        # if the argument "--restart" is passed, restart all containers and clean old images
+        if "--restart" in args.args:
+            run(split(
+                f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} down --remove-orphans'))
+            run(split(
+                f'{DOCKER_COMPOSE_COMMAND} {docker_compose_args} up -d'))
+            remove_old_docker_images()
     elif args.command == "prune":
         run(split('docker builder prune -f'))
         run(split('docker container prune -f'))
